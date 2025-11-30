5b9277d9a569b2c50e40d70da4788c2c754eb780
enchevri <enzochevrier@icloud.com>
2025-11-30 23:17:31 +0100

    Last commit

--- src/ui/ui.c
+++ src/ui/ui.c
@@ -6,7 +6,7 @@
 /*   By: enchevri <enchevri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/30 10:11:48 by enchevri          #+#    #+#             */
-/*   Updated: 2025/11/30 18:56:56 by enchevri         ###   ########lyon.fr   */
+/*   Updated: 2025/11/30 23:16:53 by enchevri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	print_ui(t_game *game)
 	print_score(game);
 	print_time(game);
 	print_lives(game);
-	if (game->player.invincibility <= 0)
+	if (game->player.invincibility <= 0 || game->player.invincibility % 30 < 10)
 	{
 		render_box(score_win, 1);
 		render_box(game_win, 1);
@@ -27,20 +27,10 @@ void	print_ui(t_game *game)
 	}
 	else
 	{
-		if (game->player.invincibility % 30 < 10)
-		{
-			render_box(score_win, 1);
-			render_box(game_win, 1);
-			render_box(time_win, 1);
-			render_box(lives_win, 1);
-		}
-		else
-		{
-			render_box(score_win, 3);
-			render_box(game_win, 3);
-			render_box(time_win, 3);
-			render_box(lives_win, 3);
-		}
+		render_box(score_win, 3);
+		render_box(game_win, 3);
+		render_box(time_win, 3);
+		render_box(lives_win, 3);
 	}
 	wrefresh(score_win);
 	wrefresh(time_win);
